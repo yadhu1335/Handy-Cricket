3a265aeefdb86b915e39783b7bb8859bc34f9771
yadhu1335 <yadhu1335@gmail.com>
2024-03-16 18:41:31 +0530

    The code is working(i think). Next shoud focus on little bit of html and finally the styling

--- game.js
+++ game.js
@@ -1,20 +1,113 @@
-//code from index.js
-// let batorbowl = "bat";
-let batorbowl = "bowling";
-// console.log("batorbowl set to bat explicitly");
-console.log("batorbowl set to bowl explicitly");
-
 // let youare = "bat";
-let youare = "bowling";
+
+// let batorbowl=""
+// let youare = "bowling";
 
 let bat = youare == "bat" ? true : false;
 let bowl = bat == true ? false : true;
 
 let ch = ["1", "2", "3", "4", "5", "6"]; //choice for battobowl
-// let out = false;
 
-// let prevsel = [];
 let score = 1;
+// function play(val) {
+//   int_val = parseInt(val);
+//   console.log("user input:" + int_val);
+//   if (youare == "bat") {
+//     //function bat to bowl
+//     //if bat == true then user is batting else he is balling
+//     if (bat) {
+//       //user is batting
+//       const random = ch[Math.floor(Math.random() * 6)];
+//       console.log("COMPUTERS CHOICE=" + random);
+//       if (val == random) {
+//         console.log("YOU ARE OUT");
+//         console.log("SCORE to win=" + score);
+//         bat = false;
+//         bowl = true;
+//       } else score = score + int_val;
+//       console.log("CURRENT SCORE=" + score);
+//     } else {
+//       // user is bowling
+//       console.log("You are now bowling");
+
+//       if (score > 0) {
+//         const random = ch[Math.floor(Math.random() * 6)];
+//         console.log("COMPUTERS CHOICE(bowl)=" + random);
+
+//         if (val == random) {
+//           console.log("you win");
+//           alert("YOU WIN!!");
+//           document.querySelectorAll(".game-button").forEach((button) => {
+//             button.disabled = true;
+//           });
+//           return;
+//         } else {
+//           if (score > int_val) {
+//             score = score - parseInt(random);
+//           } else {
+//             score = parseInt(random) - score;
+//           }
+//           console.log("SCORE to win(ball)=" + score);
+//         }
+//       }
+
+//       if (score <= 0) {
+//         console.log("You loose");
+//         alert("YOU LOOSE!!");
+//         console.log("score=" + score);
+//         document.querySelectorAll(".game-button").forEach((button) => {
+//           button.disabled = true;
+//         });
+//         return;
+//       }
+//     }
+//   } else {
+//     if (bowl) {
+//       const random = ch[Math.floor(Math.random() * 6)];
+//       console.log("COMPUTERS CHOICE=" + random);
+//       if (val == random) {
+//         console.log("COMPUTR IS OUT");
+//         // score += 1;
+//         console.log("SCORE to win=" + score);
+//         bowl = false;
+//         bat = true;
+//       } else score = score + parseInt(random);
+//       console.log("CURRENT SCORE=" + score);
+//     } else {
+//       if (score > 0) {
+//         const random = ch[Math.floor(Math.random() * 6)];
+//         console.log("COMPUTERS CHOICE(bat)=" + random);
+
+//         if (val == random) {
+//           console.log("you are out, YOU LOOSE");
+//           alert("YOU ARE OUT!!");
+//           document.querySelectorAll(".game-button").forEach((button) => {
+//             button.disabled = true;
+//           });
+//           return;
+//         } else {
+//           if (score > int_val) {
+//             // score = score - parseInt(random);
+//             score = score - int_val;
+//           } else {
+//             // score = parseInt(random) - score;
+//             score = int_val - score;
+//           }
+//           console.log("SCORE to win(ball)=" + score);
+//         }
+//       }
+//       if (score <= 0) {
+//         console.log("You WINN");
+//         alert("YOU WINN!!");
+//         console.log("score=" + score);
+//         document.querySelectorAll(".game-button").forEach((button) => {
+//           button.disabled = true;
+//         });
+//         return;
+//       }
+//     }
+//   }
+// }
 function play(val) {
   int_val = parseInt(val);
   console.log("user input:" + int_val);
@@ -45,7 +138,7 @@ function play(val) {
           document.querySelectorAll(".game-button").forEach((button) => {
             button.disabled = true;
           });
-          return;
+          // return;
         } else {
           if (score > int_val) {
             score = score - parseInt(random);
@@ -81,7 +174,7 @@ function play(val) {
     } else {
       if (score > 0) {
         const random = ch[Math.floor(Math.random() * 6)];
-        console.log("COMPUTERS CHOICE(bat)=" + random);
+        console.log("COMPUTERS CHOICE aganist you=" + random);
 
         if (val == random) {
           console.log("you are out, YOU LOOSE");
@@ -89,14 +182,15 @@ function play(val) {
           document.querySelectorAll(".game-button").forEach((button) => {
             button.disabled = true;
           });
-          return;
+          // return;
         } else {
           if (score > int_val) {
             // score = score - parseInt(random);
             score = score - int_val;
           } else {
             // score = parseInt(random) - score;
-            score = int_val - score;
+            // score = int_val - score;
+            score = 0;
           }
           console.log("SCORE to win(ball)=" + score);
         }
@@ -113,94 +207,3 @@ function play(val) {
     }
   }
 }
-
-// let leastpredict;
-// let prediction;
-
-//user goes from bat to bowl
-// function battobowl(val) {
-//   while (prevsel.length < 4) {
-//     console.log("length of prevsel=" + prevsel.length);
-//     //while the length of array is less than 4. It chooses a random number from  0-5 and pushes it into the prevsel array.
-//     if (ch[Math.floor(Math.random() * 6)] == val) {
-//       console.log("OUT");
-//       // alert("OUT!!");
-//       out = true;
-//     } else score += val;
-//     prevsel.push(val);
-//   }
-//   //if prev.lengh > 3
-//   while (out == false) {
-//     // let prediction = predict(prevsel);
-//     prediction = predict(prevsel); //computers prediction
-//     console.log("computer prediction=" + prediction);
-
-//     if (prediction == val) {
-//       console.log("OUT");
-//       // alert("OUT!!");
-//       out = true;
-//     } else score += val;
-//     prevsel.push(val);
-//   }
-//   //bowling
-//   // let leastpredict = predictLeast(prevsel);
-//   console.log("Switching to bowling");
-//   while (score > 0) {
-//     leastpredict = predictLeast(prevsel);
-//     console.log("Least predict(for bowling)=" + leastpredict);
-//     if (val !== leastpredict) {
-//       score = score - val;
-//     } else {
-//       // alert("YOU WON!!!");
-//       console.log("You won!");
-//       return;
-//     }
-//   }
-//   console.log("YOU LOOSE!!! THE COMPUTER BEAT YOUR SCORE");
-// }
-
-// function predict(prevsel) {
-//   // Frequency of each choice in the previous selections
-//   const freq = {};
-//   ch.forEach((c) => {
-//     freq[c] = 0;
-//   });
-//   prevsel.forEach((c) => {
-//     freq[c]++;
-//   });
-
-//   // Find the choice with the highest frequency
-//   let maxFreq = -Infinity;
-//   let mostChoice = null;
-//   for (let key in freq) {
-//     if (freq[key] > maxFreq) {
-//       maxFreq = freq[key];
-//       mostChoice = key;
-//     }
-//   }
-
-//   return mostChoice;
-// }
-
-// function predictLeast(prevsel) {
-//   // Frequency of each choice in the previous selections
-//   const freq = {};
-//   ch.forEach((c) => {
-//     freq[c] = 0;
-//   });
-//   prevsel.forEach((c) => {
-//     freq[c]++;
-//   });
-
-//   // Find the choice with the lowest frequency
-//   let leastFreq = Infinity;
-//   let leastChoice = null;
-//   for (let key in freq) {
-//     if (freq[key] < leastFreq) {
-//       leastFreq = freq[key];
-//       leastChoice = key;
-//     }
-//   }
-
-//   return leastChoice;
-// }
--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function check() {
     document.getElementById("computerchoice").innerText = batorbowl;
     computerchoice = true;
 
-    youare = batorbowl === "bat" ? "bowling" : "bat";
+    youare = batorbowl == "bat" ? "bowling" : "bat";
     console.log("you are now " + youare);
   }
   // window.bob = batorbowl;
@@ -71,104 +71,5 @@ function check() {
   document.getElementById("currently").innerText = youare;
 }
 
-console.log("batorbowl set to bat explicitly");
-
-//from game.js
-let prevsel = [];
-let score = 0;
-function play(val) {
-  // prevsel.push(val);// //we shouldny push the val the first time. Should update it after checking if it is working.
-  console.log("user input:" + val);
-
-  if (youare === "bat") {
-    batorbowl(val);
-  } else {
-    // bowltobat();
-    battobowl(val); //debugging purpose
-  }
-}
-
-let leastpredict;
-let prediction;
-//user goes from bat to bowl
-function battobowl(val) {
-  out = false;
-  while (prevsel.length < 4) {
-    //while the length of array is less than 4. It chooses a random number from  0-5 and pushes it into the prevsel array.
-    let ch = ["1", "2", "3", "4", "5", "6"];
-    if (ch[Math.floor(Math.random() * 6)] === val) {
-      console.log("OUT");
-      alert("OUT!!");
-      out = true;
-    } else score += 1;
-    prevsel.push(val);
-  }
-  //if prev.lengh > 3
-  while (out == false) {
-    // let prediction = predict(prevsel);
-    prediction = predict(prevsel);
-    console.log("computer prediction=" + prediction);
-
-    if (prediction == val) {
-      console.log("OUT");
-      alert("OUT!!");
-      out = true;
-    }
-    prevsel.push(val);
-  }
-  //bowling
-  // let leastpredict = predictLeast(prevsel);
-  while (score > 0) {
-    leastpredict = predictLeast(prevsel);
-    console.log("Least predict(for bowling)=" + leastpredict);
-    if (val !== leastpredict) {
-      score = score - val;
-    } else {
-      alert("YOU WON!!!");
-      console.log("You won!");
-    }
-  }
-}
-
-//function to predict the most  probable number
-function predict(selections) {
-  let freeqmap = {};
-  selections.forEach((selection) => {
-    freeqmap[selection] = (freeqmap[selection] || 0) + 1;
-  });
-
-  let prediction = null;
-  let maxfreeq = null;
-
-  for (let number in freeqmap) {
-    if (freeqmap[number] > maxfreeq) {
-      maxfreeq = number;
-      prediction = freeqmap[number];
-    }
-  }
-  console.log("prediction=" + prediction);
-  console.log("maxfreeq=" + maxfreeq);
-  return prediction;
-}
-
-//functtion to choose the one with least probbility.
-function predictLeast(selections) {
-  let frequencyMap = {};
-  selections.forEach((selection) => {
-    frequencyMap[selection] = (frequencyMap[selection] || 0) + 1;
-  });
-
-  let leastFrequency = Infinity; // Initialize with Infinity to find the minimum frequency
-  let prediction = null;
-
-  for (let number in frequencyMap) {
-    if (frequencyMap[number] < leastFrequency) {
-      leastFrequency = frequencyMap[number];
-      prediction = number;
-    }
-  }
-
-  console.log("Least frequent number:", prediction);
-  console.log("Least frequency:", leastFrequency);
-  return prediction;
-}
+// window.var1 = batorbowl;
+// window.var2 = youare;
