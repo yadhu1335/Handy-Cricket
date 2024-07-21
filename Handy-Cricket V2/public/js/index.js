56efd8476b99b32560ebcb6bd9cbba55bc6942e2
yadhu1335 <yadhu1335@gmail.com>
2024-07-21 11:15:39 +0530

    added "searching..." trying to blur out others

--- Handy-Cricket V2/public/js/index.js
+++ Handy-Cricket V2/public/js/index.js
@@ -1,29 +1,41 @@
 console.log("js working");
+const main_part = document.getElementById("main_part");
 const room_id_form = document.getElementById("room_id_form");
 const room_id_input = document.getElementById("room_id"); //The input field where room id is recieved
+const join_button = document.getElementById("join_button");
 const create_room = document.getElementById("create_room");
 const bot = document.getElementById("bot");
 const tutorial = document.getElementById("tutorial");
+const random_room = document.getElementById("random_room");
+const searching_div = document.getElementById("searching_div");
+const searching_text = document.getElementById("searching_text");
+const searching_div_buttons = document.getElementById("searching_div_buttons");
 
-// Creates a value. invoke when user creates a room
-function uuidv4() {
-  return "xxyxyxxyx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
+const Socket = io();
 
 room_id_form.addEventListener("submit", (event) => {
   event.preventDefault();
   const room_id = room_id_input.value;
   console.log(room_id);
-  location.href = `/room/${room_id}`; //Routing to the room
+  //checking if the room exists in the server
+  if (room_id == "") {
+    console.log(`empty room id`);
+    alert("Invalid Room ID");
+  } else {
+    Socket.emit("room_exist", room_id);
+  }
+  Socket.on("room_exist", (boolean_value) => {
+    if (boolean_value) {
+      console.log(`${room_id} exists`);
+      location.href = `/room/${room_id}`; //Routing to the room
+    } else alert("Room does not exist");
+  });
 });
 
 create_room.addEventListener("click", () => {
   const room_id = uuidv4();
   console.log(`${room_id}`);
+  Socket.emit("create_room", room_id);
   location.href = `/room/${room_id}`; //Routing to the room
 });
 
@@ -34,3 +46,88 @@ bot.addEventListener("click", () => {
 tutorial.addEventListener("click", () => {
   location.href = `/howtoplay`; //Routing to the bot
 });
+
+random_room.addEventListener("click", () => {
+  enable_disable__button(
+    "disabled",
+    create_room,
+    bot,
+    random_room,
+    join_button
+  );
+
+  // Blur all elements inside mainPart except searchingDiv
+  Array.from(main_part.children).forEach((child) => {
+    if (child !== searching_div) {
+      child.classList.add("blur");
+    }
+  });
+
+  // creating "searching..." inside searching_div
+  // const pElement = document.createElement("p");
+  searching_text.textContent = "Searching...";
+  document.getElementById("searching_div").appendChild(searching_text);
+
+  function updateEllipsis() {
+    const currentText = searching_text.textContent;
+    searching_text.textContent = currentText.endsWith("...")
+      ? "Searching"
+      : currentText + ".";
+  }
+
+  // Start updating ellipsis dynamically. Calls every 500ms
+  const ellipsisInterval = setInterval(updateEllipsis, 500);
+
+  createTag(
+    "button",
+    "Cancel",
+    searching_div_buttons,
+    "quit_button",
+    "button-56"
+  );
+  createTag("button", "Bot", searching_div_buttons, "bot_button", "button-56");
+  Socket.emit("random_room");
+  Socket.on("assigned_room_id", (room_id) => {
+    clearInterval(ellipsisInterval);
+    document.getElementById("searching_div").style.display = "none";
+    location.href = `/room/${room_id}`;
+  });
+});
+
+//functions starts here
+// Creates a value. invoke when user creates a room
+function uuidv4() {
+  return "xxyxyxxyx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+//function to add tags to html
+//eg: createTag("<tag like button,p,h1-6 etc>", "msg u want to display", div where you want to place the tag, "id for the tag(not necessary)");
+function createTag(tagname, message, parentDiv, id = null, className = null) {
+  const tag = document.createElement(tagname);
+  tag.textContent = message;
+  if (id) {
+    tag.id = id;
+  }
+  if (className) {
+    tag.className = className;
+  }
+  parentDiv.appendChild(tag);
+}
+
+function enable_disable__button(enable_or_disable, ...buttons) {
+  if (enable_or_disable == "disabled") {
+    buttons.forEach((button) => {
+      button.style.cursor = "not-allowed";
+      button.disabled = true;
+    });
+  } else {
+    buttons.forEach((button) => {
+      button.style.cursor = "pointer";
+      button.disabled = false;
+    });
+  }
+}
--- Handy-Cricket V2/server.js
+++ Handy-Cricket V2/server.js
@@ -25,6 +25,44 @@ app.get("/howtoplay", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "views", "tutorial.html"));
 });
 
+let exisiting_room = []; //array containing ll the rooms that exists.used for validation when user enters room id and joins.
+let Rooms = {};
+let random_room = []; // for saving socket.id of people who want to play aganist random people.
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+  socket.on("room_exist", (room_id) => {
+    if (exisiting_room.includes(room_id)) {
+      console.log(`${room_id} exists`);
+      io.to(socket.id).emit("room_exist", true);
+    } else {
+      console.log(`${room_id} does not exist`);
+      io.to(socket.id).emit("room_exist", false);
+    }
+  });
+
+  //creating room
+  socket.on("create_room", (room_id) => {
+    add_room_id(room_id);
+  });
+
+  socket.on("random_room", () => {
+    random_room.push(socket.id);
+    console.log(`${socket.id} has been added to the array randomroom`);
+    if (random_room.length >= 2) {
+      console.log("found 2 people");
+      room_id = uuidv4(); //generates a roomid
+      add_room_id(room_id); //adding and initializing newly created room_id
+      for (let i = 0; i < 2; i++) {
+        io.to(random_room[i]).emit("assigned_room_id", room_id);
+      }
+      random_room.shift();
+      random_room.shift();
+      //removing the first 2 element from the array
+    }
+  });
+});
+
 const asciiArt = `
 ██╗   ██╗ █████╗ ██████╗ ██╗  ██╗██╗   ██╗
 ╚██╗ ██╔╝██╔══██╗██╔══██╗██║  ██║██║   ██║
@@ -37,3 +75,28 @@ server.listen(PORT, () => {
   console.log(asciiArt);
   console.log(`Server is up and running on port http://localhost:${PORT}`);
 });
+
+function add_room_id(room_id) {
+  console.log(`added room ${room_id} to the array`);
+  exisiting_room.push(room_id); //adding the room to the array
+  Rooms[room_id] = {
+    userCount: 0,
+    toss: {},
+    buffer: {},
+    // scores: {},
+    // // game is used to save the scores of the socket.is's
+    // switch_side: false,
+    // bat: 0,
+    // bowl: 0,
+    // //bat and bowl is used to store the bat and bowl value
+    // score_to_beat: 0,
+  }; // Initialize room with empty users array and user count 0
+}
+
+function uuidv4() {
+  return "xxyxyxxyx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
